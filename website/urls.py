d0d44f272957427442641b7b617150ead36dad10
sareh6142 <102669191+sareh6142@users.noreply.github.com>
2024-09-30 15:54:05 +0330

    index/static

--- website/urls.py
+++ website/urls.py
@@ -4,8 +4,8 @@ from .views import home_view,contact_view,about_view,licenses_view
 
 urlpatterns = [
     
-    path('',home_view,name='home')
+    path('',home_view,name='home'),
     path('contact/',contact_view,name = 'contact'),
     path('about/',about_view, name = 'about'),
-    path('licences',licenses_view, name = 'licenses')
+    path('licences',licenses_view, name = 'licenses'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home_view():
-    pass
+def home_view(request):
+    return render(request,'website/index.html')
 
 
 def contact_view():
